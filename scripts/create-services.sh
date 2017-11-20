a2a73db45e0b2cf66599fbe56ec249b86f0101a9
erwanbornier <ebornier@pivotal.io>
2017-11-20 22:02:14 +0100

    update scripts

--- scripts/create-services.sh
+++ scripts/create-services.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 export
-cf login -u $CF_USERNAME -p $CF_PASSWORD -a $CF_API -o prod -s $CF_ORG --skip-ssl-validation
+cf login -u $CF_USERNAME -p $CF_PASSWORD -a $CF_API -o$CF_ORG -s prod  --skip-ssl-validation
 
-cf target -o prod 
 cf create-service metrics-forwarder unlimited ${APP_NAME}-metrics
 cf bind-service ${APP_NAME} ${APP_NAME}-metrics
