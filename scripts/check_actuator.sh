8ca8ee82be4bf02834def3867331ed3d4079c1a9
erwanbornier <ebornier@pivotal.io>
2017-11-22 23:18:50 +0100

    update scripts

--- scripts/check_actuator.sh
+++ scripts/check_actuator.sh
@@ -1,6 +1,9 @@
 #!/bin/sh 
 
 echo "Performed source code analysis" 
+
+apk add --no-cache curl
+
 metric=`curl http://spring-boot-paris.cfapps.alex.examples.cf/metrics | jq -r '.["counter.spring.paris.attendees.view"]'`
 echo $metric
 if [ $metric > 10 ] 
