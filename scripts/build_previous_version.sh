14b6a14ceeb82e89433198d6be6e80579498b019
erwanbornier <ebornier@pivotal.io>
2017-11-24 09:32:11 +0100

    update scripts

---
+++ scripts/build_previous_version.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+
+tarball_url=`curl https://api.github.com/repos/ebornier-pivotal/spring-paris-boot/releases/tags/V1.0.0 | jq -r ".tarball_url"
+wget -O release.tar.gz $tarball_url
+
+tar xvf release.tar.gz --strip 1
+export
+mvn clean package -Dmaven.test.skip=true
+
+cat << EOF > manifest.yml
+---
+applications:
+- name: ${APP_NAME} 
+  host: ${APP_NAME}
+  memory: 1G
+  instances: 3
+  services:
+    - ${APP_NAME}-metrics
+EOF
+
+cat manifest.yml
+
+mv manifest.yml ../build/
+mv `ls target/*jar` ../build/app.jar
