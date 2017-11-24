0e849f0082ace106fdccfaffd0143cec50ff27b6
erwanbornier <ebornier@pivotal.io>
2017-11-24 09:55:57 +0100

    update scripts

--- scripts/retrieve_previous_version.sh
+++ scripts/retrieve_previous_version.sh
@@ -3,8 +3,8 @@
 ls
 cat check-actuator/rollback
 
-tarball_url=`curl https://api.github.com/repos/ebornier-pivotal/spring-paris-boot/releases/tags/V1.0.0 | jq -r ".tarball_url"`
-echo $tarball_url
+tarball_url=`curl "https://api.github.com/repos/ebornier-pivotal/spring-paris-boot/releases/tags/V1.0.0" | jq -r ".tarball_url"`
+echo "test" $tarball_url
 curl -O  $tarball_url
 
 mv "V1.0.0" release.tar.gz
